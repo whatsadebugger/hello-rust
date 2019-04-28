be01ab73bef53480bfdf1e73b60f92e17b486a13
Tabbakha, Ahmad <ahmad.tabbakha@toyotaconnected.com>
2019-04-28 17:25:41 -0500

    add comments to add rs programs

--- add1.rs
+++ add1.rs
@@ -1,5 +1,6 @@
 // add1.rs
 fn main() {
+    // sum is immutable because we are missing the mut keyword
     let sum = 0;
     for i in 0..5 {
         sum += i;
--- add3.rs
+++ add3.rs
@@ -1,7 +1,9 @@
 // add3.rs
 fn main() {
+    // with rust type inference this is not a float
     let mut sum = 0.0;
     for i in 0..5 {
+        // without this cast we get an error
         sum += i as f64;
     }
     println!("sum is {}", sum);
