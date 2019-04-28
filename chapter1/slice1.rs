a6ac90669f22b2b7838d438508c3c6a4ffcc576a
Tabbakha, Ahmad <ahmad.tabbakha@toyotaconnected.com>
2019-04-28 18:47:47 -0500

    add array2,3 slice1

---
+++ chapter1/slice1.rs
@@ -0,0 +1,11 @@
+// slice1.rs
+fn main() {
+    let ints = [1, 2, 3, 4, 5];
+    let slice1 = &ints[0..2];
+    let slice2 = &ints[1..];  // open range!
+
+    println!("ints {:?}", ints);
+    println!("slice1 {:?}", slice1);
+    println!("slice2 {:?}", slice2);
+}
+// When slicing an array a copy of the data is never made. These slices all borrow their data from their arrays.
